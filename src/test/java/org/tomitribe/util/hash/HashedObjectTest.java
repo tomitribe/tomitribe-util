36a2525c34def1fd8d34474a4f61ca2e216111dd
dblevins <david.blevins@gmail.com>
2014-08-19 15:58:52 -0700

    Tests for XxHash64 and XxHash32

---
+++ src/test/java/org/tomitribe/util/hash/HashedObjectTest.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.tomitribe.util.hash;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.tomitribe.util.Longs;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashedObjectTest extends Assert {
+
+
+    @Test
+    public void test() throws Exception {
+
+        assertEquals(59, data(new Hasher() {
+            @Override
+            public long hash(String value) {
+                return XxHash32.hash(value);
+            }
+        }).conflicts);
+
+
+        assertEquals(0, data(new Hasher() {
+            @Override
+            public long hash(String value) {
+                return XxHash64.hash(value);
+            }
+        }).conflicts);
+
+    }
+
+    private static class Data {
+        private int conflicts;
+        private int strings;
+        private long time;
+
+        @Override
+        public String toString() {
+            return "Data{" +
+                    "strings=" + strings +
+                    ", time=" + (time / strings) +
+                    ", conflicts=" + conflicts +
+                    '}';
+        }
+    }
+
+    private Data data(Hasher hasher) {
+        final Data data = new Data();
+
+        data.conflicts = 0;
+        data.strings = 1000000;
+
+        final long[] longs = new long[data.strings];
+
+        final long start = System.nanoTime();
+        for (int i = 0; i < data.strings; i++) {
+            final String value = "http://stackoverflow.com/questions/" + i + "/convert-from-byte-array-to-hex-string-in-java";
+            longs[i] = hasher.hash(value);
+        }
+        data.time = System.nanoTime() - start;
+
+        {
+            final Set<String> strings = new HashSet<String>();
+            for (final long l : longs) {
+                final String s = Longs.toHex(l);
+                if (!strings.add(s)) {
+                    data.conflicts++;
+                }
+            }
+        }
+        return data;
+    }
+
+}
---
+++ src/test/java/org/tomitribe/util/hash/XxHash32Test.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.tomitribe.util.hash;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.tomitribe.util.Longs;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class XxHash32Test extends Assert {
+
+    final Hasher xxhash32 = new Hasher() {
+        @Override
+        public long hash(String value) {
+            return XxHash32.hash(value);
+        }
+    };
+
+
+    @Test
+    public void shortString() {
+        final Data data = data(xxhash32, 1000000, "http://host%s.foo.com");
+        assertEquals(22, data.conflicts);
+    }
+
+    @Test
+    public void mediumString() {
+        final Data data = data(xxhash32, 1000000, "http://stackoverflow.com/questions/%s/convert-from-byte-array-to-hex-string-in-java");
+        assertEquals(59, data.conflicts);
+    }
+
+    @Test
+    public void longerString() {
+        final Data data = data(xxhash32, 1000000, "\"Lorem ipsum\" (a.k.a. \"Lipsum\") is a popular placeholder often used in typography and web design. At its best, \"Lorem ipsum\" is a completely incomprehensible text which, to an untrained eye, may very well look like Latin. Be assured, Latin it is not. The text begins at half word as a quote from from Cicero's \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil):\n" +
+                "\n" +
+                "nemo enim ipsam voluptatem, quia voluptas sit,%s aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem.");
+        assertEquals(118, data.conflicts);
+    }
+
+    private static class Data {
+        private int conflicts;
+        private int strings;
+        private long time;
+
+        @Override
+        public String toString() {
+            return "Data{" +
+                    "strings=" + strings +
+                    ", time=" + (time / strings) +
+                    ", conflicts=" + conflicts +
+                    '}';
+        }
+    }
+
+    private Data data(Hasher hasher, final int count, final String format) {
+        final Data data = new Data();
+
+        data.conflicts = 0;
+        data.strings = count;
+
+        final long[] longs = new long[data.strings];
+
+        final long start = System.nanoTime();
+        for (int i = 0; i < data.strings; i++) {
+            final String value = String.format(format, i);
+            longs[i] = hasher.hash(value);
+        }
+        data.time = System.nanoTime() - start;
+
+        {
+            final Set<String> strings = new HashSet<String>();
+            for (final long l : longs) {
+                final String s = Longs.toHex(l);
+                if (!strings.add(s)) {
+                    data.conflicts++;
+                }
+            }
+        }
+        return data;
+    }
+
+}
