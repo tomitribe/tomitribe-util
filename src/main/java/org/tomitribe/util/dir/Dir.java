a58531eeb39ca16e82ea2eafa9bef99ef041f728
David Blevins <david.blevins@gmail.com>
2020-08-30 16:34:59 -0700

    Fixes default methods in java 11 and later

--- src/main/java/org/tomitribe/util/dir/Dir.java
+++ src/main/java/org/tomitribe/util/dir/Dir.java
@@ -279,7 +279,7 @@ public interface Dir {
                         .findSpecial(
                                 method.getDeclaringClass(),
                                 method.getName(),
-                                MethodType.methodType(method.getReturnType(), new Class[0]),
+                                MethodType.methodType(method.getReturnType(), method.getParameterTypes()),
                                 method.getDeclaringClass()
                         ).bindTo(proxy)
                         .invokeWithArguments(args);
