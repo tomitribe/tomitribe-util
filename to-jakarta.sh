c55fb03a6e28e126578d4c6e856003671f99019e
Jean-Louis Monteiro <jlmonteiro@tomitribe.com>
2023-06-19 18:11:23 +0200

    Small tweaks for the conversion and tests

--- to-jakarta.sh
+++ to-jakarta.sh
@@ -2,10 +2,9 @@
 
 # java sources - brutal but we only use JAXB annotations so no need to deal with exceptions
 find . -type f -name '*.java' -exec sed -i'' -e 's/javax./jakarta./g' {} +
+find . -type f -name '*.java' -exec sed -i'' -e 's/jakarta.xml.parsers./javax.xml.parsers./g' {} +
 # service loader files
 find . -path "*/src/main/resources/META-INF/services/javax*" | sed -e 'p;s/javax/jakarta/g' | xargs -n2 git mv
-# docs
-find documentation -type f -name '*.md' -exec sed -i'' -e 's/javax./jakarta./g' {} +
 
 mvn -ntp build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT
 
