93b8f0915c918aa6b7c69e9299b274f8f0fe1728
David Blevins <dblevins@tomitribe.com>
2025-08-27 12:00:52 -0700

    Update license header

--- src/test/java/org/tomitribe/util/FuturesTest.java
+++ src/test/java/org/tomitribe/util/FuturesTest.java
@@ -1,6 +1,4 @@
 /*
- * Copyright 2022 Tomitribe and community
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,6 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.tomitribe.util;
 
 import org.junit.Test;
